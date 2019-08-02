e02c93f2cf6209260f81d35317539d4e7ed07112
Shayla Lane <lanesha@oregonstate.edu>
2019-08-02 11:36:31 -0700

    Obtain port from environment

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // Foster Community Connect Server
 const express = require('express')
 const app = express()
-const port = 3000
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port);
