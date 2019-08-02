cabd88fa29498f44a244a4d3844b86f8ef57fcd4
Shayla Lane <lanesha@oregonstate.edu>
2019-08-02 12:14:52 -0700

    Add username and password via .env

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // Foster Community Connect Server
+require('dotenv').config()
 const express = require('express')
 const app = express()
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
+const gmail_username = process.env.GMAIL_USERNAME;
+const gmail_password = process.env.GMAIL_PASSWORD;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(req, res) {
+    res.send(gmail_password);
+});
 
 app.listen(port);
