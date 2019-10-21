9ebcb93e618e8b81794de9bf950b2ead9b54dca6
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-10-21 16:13:03 -0700

    Format and extract url and destination email into config.

--- index.js
+++ index.js
@@ -20,40 +20,30 @@ var transporter = nodemailer.createTransport({
   }
 })
 
+const route_path = process.env.ROUTE_PATH
+const recipient_email_address = process.env.RECIPIENT_EMAIL_ADDRESS
 
-var name;
+app.get(route_path, function(req, res) {
+  var name = req.query.firstName + ' ' + req.query.lastName
+  var emailAddress = req.query.email
+  var phone = req.query.phoneNumber
+  var items = req.query.description
 
-app.get('/ff792xyp872', function(req, res) {
-    var name = req.query.firstName + ' ' + req.query.lastName;
-    var emailAddress = req.query.email;
-    var phone = req.query.phoneNumber;
-    var items = req.query.description;
+  var mailOptions = {
+    from: emailAddress,
+    to: recipient_email_address,
+    subject: 'Request from ' + name,
+    text: name + ' is requesting: ' + items + '\n\n' + name +"'s phone number is " + phone + '\n' + name + "'s email address is " + emailAddress
+  }
 
-    var mailOptions = {
-      from: emailAddress,
-      to: 'shaylalane522@gmail.com',
-      subject: 'Request from ' + name,
-      text: name + ' is requesting: ' + items + '\n\n' + name +"'s phone number is " + phone + '\n' + name + "'s email address is " + emailAddress
-    };
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error)
+      res.send("SMTP log:" + error.data)
+    } else {
+      res.send('Congratulations you little genius. Email sent:' + info.response)
+    }
+  })
+})
 
-
-
-    transporter.sendMail(mailOptions, function(error, info){
-        if(error) {
-            console.log(error);
-            res.send("SMTP log:" + error.data);
-        } else {
-
-            res.send('Congratulations you little genius. Email sent:' + info.response);
-            console.log(req.originalUrl);
-            for (const key in req.query) {
-              console.log(key, req.query[key])
-            }
-
-            var first = req.query.firstName;
-            console.log(first);
-        }
-    });
-});
-
-app.listen(port);
+app.listen(port)
