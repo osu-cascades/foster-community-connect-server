68126302086f7b94ad2697090398bd0d308211ff
Shayla Lane <lanesha@oregonstate.edu>
2019-11-16 15:17:51 -0800

    Remove nodemailer

--- index.js
+++ index.js
@@ -51,28 +51,28 @@ app.get('/mailgun', function(req, res) {
 })
 
 
-app.get(route_path, function(req, res) {
-  var name = req.query.firstName + ' ' + req.query.lastName
-  var emailAddress = req.query.email
-  var phone = req.query.phoneNumber
-  var items = req.query.description
+// app.get(route_path, function(req, res) {
+//   var name = req.query.firstName + ' ' + req.query.lastName
+//   var emailAddress = req.query.email
+//   var phone = req.query.phoneNumber
+//   var items = req.query.description
 
-  var mailOptions = {
-    from: emailAddress,
-    to: recipient_email_address,
-    subject: 'Request from ' + name,
-    text: name + ' is requesting: ' + items + '\n\n' + name +"'s phone number is " + phone + '\n' + name + "'s email address is " + emailAddress
-  }
+//   var mailOptions = {
+//     from: emailAddress,
+//     to: recipient_email_address,
+//     subject: 'Request from ' + name,
+//     text: name + ' is requesting: ' + items + '\n\n' + name +"'s phone number is " + phone + '\n' + name + "'s email address is " + emailAddress
+//   }
 
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      console.log(error)
-      res.send("SMTP log:" + error.data)
-    } else {
-      res.send('Congratulations you little genius. Email sent:' + info.response)
-    }
-  })
-})
+//   transporter.sendMail(mailOptions, function(error, info) {
+//     if (error) {
+//       console.log(error)
+//       res.send("SMTP log:" + error.data)
+//     } else {
+//       res.send('Congratulations you little genius. Email sent:' + info.response)
+//     }
+//   })
+// })
 
 
 app.listen(port)
