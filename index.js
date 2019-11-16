7d38f436ceb1f80ac7a659b30561b78e7d42bd13
Shayla Lane <lanesha@oregonstate.edu>
2019-11-16 15:00:53 -0800

    Add querystring variables to mailgun mailoptions

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ var nodemailer = require('nodemailer')
 const express = require('express')
 const cors = require('cors')
 const app = express()
-var api_key = process.env.MAILGUN_API_KEY;
-var domain = process.env.MAILGUN_DOMAIN
-var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain})
-
-app.use(cors())
-
 let port = process.env.PORT
 if (port == null || port == '') {
   port = 3000
 }
+const api_key = process.env.MAILGUN_API_KEY;
+const domain = process.env.MAILGUN_DOMAIN
+const route_path = process.env.ROUTE_PATH
+const recipient_email_address = process.env.RECIPIENT_EMAIL_ADDRESS
+
+const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain})
+
+app.use(cors())
+
 
 var transporter = nodemailer.createTransport({
   service: 'Gmail',
@@ -23,16 +26,18 @@ var transporter = nodemailer.createTransport({
   }
 })
 
-const route_path = process.env.ROUTE_PATH
-const recipient_email_address = process.env.RECIPIENT_EMAIL_ADDRESS
-
 //mailgun version of transporter
 app.get('/mailgun', function(req, res) {
+  var name = req.query.firstName + ' ' + req.query.lastName
+  var emailAddress = req.query.email
+  var phone = req.query.phoneNumber
+  var items = req.query.description
+
   var mailOptions = {
-    from: 'example@example.com',
-    to: 'shaylalane522@gmail.com',
-    subject: 'TEST',
-    text: 'TESTOLA'
+    from: emailAddress,
+    to: recipient_email_address,
+    subject: 'Request from ' + name,
+    text: name + ' is requesting: ' + items + '\n\n' + name +"'s phone number is " + phone + '\n' + name + "'s email address is " + emailAddress
   }
 
   mailgun.messages().send(mailOptions, function (error, body) {
