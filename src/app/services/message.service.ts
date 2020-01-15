62835b353f2f7ea0cfe98100e136e1f25f2cbc84
Yong Joseph Bakos <ybakos@humanoriented.com>
2020-01-14 17:38:25 -0800

    MessageService: Use relative URL.

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -10,6 +10,6 @@ export class MessageService {
   constructor(private http: HttpClient) { }
   async sendMessage(body) {
     console.log({body});
-    return this.http.get(`http://localhost:3000/mailgun?firstName=`+body.firstName+`&lastName=`+body.lastName+`&email=`+body.email+`&phoneNumber=`+body.phoneNumber+`&description=`+body.description+`&form_type=`+body.form_type + ``).subscribe()
+    return this.http.get(`/mailgun?firstName=`+body.firstName+`&lastName=`+body.lastName+`&email=`+body.email+`&phoneNumber=`+body.phoneNumber+`&description=`+body.description+`&form_type=`+body.form_type + ``).subscribe()
   }
 }
