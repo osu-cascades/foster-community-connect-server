557d9a3a144d2cb813493aedc44b50e0bbed3507
alexander89d <46639271+alexander89d@users.noreply.github.com>
2020-05-27 19:38:53 -0400

    Create request-form.component.spec.ts
    Since request-form.component.spec.ts was missing, created request-form.component.spec.ts with the default content generated when running "ng generate component" using the Angular CLI. Included the necessary imports for the default test to run.

---
+++ src/app/request-form/request-form.component.spec.ts
@@ -0,0 +1,27 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RequestFormComponent } from './request-form.component';
+
+describe('RequestFormComponent', () => {
+  let component: RequestFormComponent;
+  let fixture: ComponentFixture<RequestFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RequestFormComponent ],
+      imports: [ FormsModule, HttpClientTestingModule ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RequestFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
