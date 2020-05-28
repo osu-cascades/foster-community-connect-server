a288c9b5cf2c6977e68fdf56a1a473b2330932e9
alexander89d <46639271+alexander89d@users.noreply.github.com>
2020-05-28 16:16:44 -0400

    Added model-to-view and view-to-model tests to request-form.component.spec.ts.
    Added model-to-view and view-to-model tests to request-form.component.spec.ts. First new test sets the first name in the form's firstName input field and asserts that model.firstName is updated. Second new test sets model.firstName and asserts that the firstName in the input field is updated.

--- src/app/request-form/request-form.component.spec.ts
+++ src/app/request-form/request-form.component.spec.ts
@@ -21,7 +21,46 @@ describe('RequestFormComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the request form', () => {
     expect(component).toBeTruthy();
   });
+  
+  /* Test firstName view-to-model. */
+  it('should update the firstName in the model upon updating it in the input field',
+    async(() => {
+      // Wait for form input values to be initialized as empty from model.
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        // Set value of 'firstName' field to 'Joe.'
+        const newFirstNameValue = 'Joe';
+        const firstNameInput = fixture.nativeElement.querySelector('#firstName');
+        firstNameInput.value = 'Joe';
+        firstNameInput.dispatchEvent(new Event('input'));
+        
+        // Wait for updating of input field value and model to complete,
+        // and then assert the update went through correctly.
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+          expect(component.model.firstName).toBe(newFirstNameValue);
+        });
+      });
+    }
+  ));
+  
+  /* Test firstName model-to-view. */
+  it('should update the firstName in the input field upon updating it in the model',
+    async(() => {
+      // Update firstName in the model.
+      const newFirstNameValue = 'Joe';
+      component.model.firstName = newFirstNameValue;
+      
+      // Wait for the change to go through,
+      // and then assert that the value in the form field matches.
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        const firstNameInput = fixture.nativeElement.querySelector('#firstName');
+        expect(firstNameInput.value).toBe(newFirstNameValue);
+      });
+    }
+  ));
 });
